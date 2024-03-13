827540771205fa9be69d4fc096cb3823c0b66ff3
ReemBino <reembinoo@gmail.com>
2024-03-13 06:07:29 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {'Liza'} is a Web Developer from {'NewYork'}
       </h1>
     </div>
   );
 }
+
+export default Home;
